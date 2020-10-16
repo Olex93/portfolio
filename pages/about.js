48334b271eb4f8224bfda335dff1dd3bebde1fe2
Olex93 <54249175+Olex93@users.noreply.github.com>
2020-10-16 14:14:39 +0100

    tech slider

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Head from 'next/head';
 import { motion } from "framer-motion";
 import classes from "../styles/about.module.css"
+import Carousel from 'nuka-carousel';
+
 
 export default function about(props) {
-
+  const technologies = ["/images/codeLogos/react.svg", "/images/codeLogos/javascript.svg", "/images/codeLogos/html.svg", "/images/codeLogos/css.svg", "/images/codeLogos/next.svg", "/images/codeLogos/bootstrap.svg", "/images/codeLogos/github.svg", "/images/codeLogos/jquery.svg", "/images/codeLogos/mongodb.svg", "/images/codeLogos/node.svg", "/images/codeLogos/wordpress.svg", "/images/codeLogos/zoho.svg"]
   const styles = {
     CustomBody: {
       marginLeft:"240px",
@@ -70,6 +72,11 @@ export default function about(props) {
       lineHeight:"145%",
       letterSpacing:"0.7px",
       margin:"15px 10px"
+    },
+    sliderImg:{
+      maxWidth:"400px",
+      padding:"50px 0 0 10px",
+      textAlign:"left"
     }
 
 }
@@ -88,8 +95,8 @@ export default function about(props) {
           <div style={styles.leftCol}>
           <ul>  
             <li style={styles.listItem}><a style={styles.link} href="#about">About Me</a></li>
-            <li style={styles.listItem}><a style={styles.link} href="#profSum">Proffessional Summary</a></li>
             <li style={styles.listItem}><a style={styles.link} href="#famSof">Familliar Software</a></li>
+            <li style={styles.listItem}><a style={styles.link} href="#profSum">Proffessional Summary</a></li>
             <li style={styles.listItem}><a style={styles.link} href="#workExp">Work Experience</a></li>
             <li style={styles.listItem}><a style={styles.link} href="#achievements">Key Achievements</a></li>
             <li style={styles.listItem}><a style={styles.link} href="#training">Training</a></li>
@@ -100,11 +107,16 @@ export default function about(props) {
             <h2 id="about" style={styles.headings}>A bit about me</h2>
             <p style={styles.content}>Hi, I’m Alex. I’m a <strong>full stack</strong> javascript developer. I have a particular passion for the front-end. In my eyes, <strong>simplicity</strong> and <strong>usability</strong> trump flair every time. </p>
             <p style={styles.content}>I’m also an outdoors person. I love to cook. I’m a keen writer; in the process of writing my own novel. I live through my work and love to learn.    I don’t know what I would do without my partner or my friends.</p>
+            <h2  id="famSof" style={styles.headings}>Familliar software</h2>
+            <Carousel defaultControlsConfig={{nextButtonClassName: 'carouselNext'}} autoplay={true} wrapAround={true} slidesToShow={1} renderBottomCenterControls={null} renderCenterLeftControls={null} >
+              {technologies.map(logo => 
+                   <img style={styles.sliderImg} src={logo} />
+                  )
+              }
+              </Carousel>
             <h2  id="profSum" style={styles.headings}>Proffessional Summary</h2>
             <p style={styles.content}>I believe that the best way to understand something is to build it. I find an immense amount of satisfaction in that. At uni, I wrote a dissertation that struggled with the concept of universal truth. I believe this is what drew me to development. <strong>To build something is to know it.</strong></p>
             <p style={styles.content}>I have worked as a developer both on a <strong>freelance</strong> basis and within various <strong>employed</strong> roles. Whilst employed, I have spent time designing and developing WordPress websites. On a freelance basis and more recently, I have built a number of websites using the <strong>MERN</strong> (Mongoose, Express, React, Node) stack, along with other notable technologies such as Next.js, Framer Motion, Material UI, Axios, Heroku and more. </p>
-            <h2  id="famSof" style={styles.headings}>Familliar software</h2>
-
             <h2  id="workExp" style={styles.headings}>Work Experience</h2>
             <h2  id="achievements" style={styles.headings}>Key Achievements</h2>
             <h2  id="training" style={styles.headings}>Training</h2>
